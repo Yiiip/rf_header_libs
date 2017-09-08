5c791bbb4a3ef0af9c852663d04191355d8dc27c
d3l1x <ryan.j.fleury@gmail.com>
2017-09-08 12:52:59 -0600

    Updated functionality
    - _da_grow now can grow to an arbitrary size- it doesn't simply grow for 1 element anymore. This will help for future functionality (union of two arrays, etc.)
    - Removed da_shrink, wasn't happy with implementation... I'll redo this later
    - Removed redundant functions (da_push now uses _da_insert and da_pop now uses _da_erase)
    - Replaced trivial functions (like da_size or da_cap) with macros

--- darray.h
+++ darray.h
@@ -1,94 +1,101 @@
 /*
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-SINGLE-HEADER C/++ DYNAMIC ARRAY LIBRARY
-	
-	---DESCRIPTION---
-	
-	This library is effectively a replacement for the C++ 
-	std::vector. It allows a dynamic array capable of "stretching";
-	it allocates memory as it needs to. This was originally a
-	learning exercise inspired by Sean Barrett's stretchy buffer 
-	library which is effectively the same thing.
-	---USAGE---
-	
-	There are 8 notable functions/macros for usual usage: da_size, 
-	da_cap, da_shrink, da_push, da_insert, da_pop, da_erase, and
-	da_free. 
-	
-	*** da_size returns the number of elements currently in the 
-		dynamic array as a uint32_t
-	
-	*** da_cap returns the number of elements the memory can store
-		without reallocating as a uint32_t
-		
-	*** da_shrink shrinks the capacity if possible.
-	
-	*** da_push pushes an element to the back of the current
-		elements
-	
-	*** da_insert inserts an element at a position
-	
-	*** da_pop removes an element from the back of the array
-	
-	*** da_erase removes an element from a particular position in
-		the array
-	
-	*** da_free frees all array memory. Should usually be called
-		when done with a dynamic array.
-		
-	To declare an array suitable for usage with these functions,
-	simply create a pointer to the type you want to store,
-	initialize it to NULL, and push/pop/insert/erase as you
-	please. Remember to call da_free before the pointer goes
-	out of scope if you don't want the memory to be allocated
-	for the lifetime of the program.
-	
-	---EXAMPLE---
-	
-		int *int_array = NULL;
-		for(int i = 0; i < 1000; i++) {
-			da_push(int_array, i);
-		}
-		da_free(int_array);
-		
-	---WARNING---
-	
-	Memory will be reallocated; therefore, a pointer to an object
-	inside of the array will not /necessarily/ stay the same. If
-	you need a reference to an object to remain constant, allocate
-	an array of pointers. Consider:
-		
-		````````````````````````````````````````````````````````````````````
-		
-		int *int_array = NULL;
-		int *first_element = NULL;
-		
-		for(int i = 0; i < 1000; i++) {
-			int a = i;
-			da_push(int_array, a);
-			if(!i) { first_element = &int_array[da_size(int_array) - 1]; }
-		}
-		
-		//use of first_element is probably invalid
-		
-		````````````````````````````````````````````````````````````````````
-		
-		int **int_ptr_array = NULL;
-		int *first_element = NULL;
-		
-		for(int i = 0; i < 1000; i++) {
-			int *a = (int *)malloc(sizeof(int));
-			*a = i;
-			if(!i) { first_element = a; }
-			da_push(int_ptr_array, a);
-		}
-		
-		//using first_element is valid
-		
-		````````````````````````````````````````````````````````````````````
-	
-	LICENSE INFORMATION IS AT THE END OF THE FILE
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+       SINGLE-HEADER C/++ DYNAMIC ARRAY LIBRARY
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    DESCRIPTION
+
+        This library provides functionality that
+        allows easy creation/usage of dynamically
+        sizing arrays. It follows a similar pattern
+        to Sean Barrett's stretchy buffer library.
+        It stretches and reallocates memory as needed
+        depending on how many objects it needs to
+        store.
+
+        Its only dependency is the CRT.
+
+    USAGE
+
+        There are a few notable functions for usual
+        usage:
+
+        * da_size     returns number of elements
+
+        * da_cap      returns number of elements
+                      allowed without reallocation.
+                      if this is surpassed, the array
+                      will require reallocation of
+                      memory.
+
+        * da_push     pushes an element to the back of
+                      the array
+
+        * da_insert   inserts an element to a position
+                      in the array
+
+        * da_pop      removes the last element of the
+                      array
+
+        * da_erase    removes an element at a specified
+                      position in the array
+
+        * da_free     frees all memory associated with
+                      an array
+
+        To declare an array ready to use with these
+        functions, simply create a pointer to a type of
+        your choice and point it to NULL. Remember to
+        call da_free before the pointer goes out of
+        scope if you don't want the memory to be
+        allocated for the lifetime of the program.
+
+    EXAMPLE
+
+        int *int_array = NULL;
+        for(int i = 0; i < 100; i++) {
+            da_push(int_array, i);
+            printf("%i\n", int_array[i]);
+        }
+        da_free(int_array);
+
+    WARNING
+
+        Memory might be reallocated. Therefore, don't
+        expect a pointer to an element to remain
+        constant. If you need pointers to elements
+        to remain consistent, consider creating an
+        array of pointers instead. Consider:
+
+            int *int_array = NULL,
+                *first_element = NULL;
+
+            for(int i = 0; i < 100; i++) {
+                da_push(int_array, i);
+                if(!i) first_element = int_array;
+            }
+
+            //first_element probably dangling
+
+        vs.
+
+            int **int_array = NULL,
+                *first_element = NULL;
+
+            for(int i = 0; i < 100; i++) {
+                int *int_ptr = (int *)malloc(sizeof(int));
+                *int_ptr = i;
+                da_push(int_array, int_ptr);
+                if(!i) first_element = int_ptr;
+            }
+
+            //first_element is not dangling
+
+        Also, do not try to insert something at a
+        position greater than the length of the
+        array.
+
+    LICENSE INFORMATION IS AT THE END OF THE FILE
 */
 
 #ifndef _DARRAY_H
@@ -101,90 +108,37 @@ SINGLE-HEADER C/++ DYNAMIC ARRAY LIBRARY
 
 #define _DARRAY_START_CAP 32
 
-#define da_shrink(a) if(da_size(a) > 0) { _da_shrink((void **)&a, sizeof(a[0])); }
-#define da_push(a, e) _da_push((void **)&a, &e, sizeof(e))
-#define da_insert(a, e, i) _da_insert((void **)&a, &e, sizeof(e), i)
-#define da_pop(a) if(da_size(a)) { _da_pop((void **)&a, sizeof(a[0])); }
-#define da_erase(a, i) if(da_size(a)) { _da_erase((void **)&a, sizeof(a[0]), i); }
-#define da_free(a) _da_free((void **)&a)
+#define da_raw(a)                       (a ? ((uint32_t *)a) - 2 : NULL)
+#define da_size(a)                      (a ? da_raw(a)[0] : 0)
+#define da_cap(a)                       (a ? da_raw(a)[1] : 0)
 
-inline uint32_t *_da_raw(void *array) {
-    return (uint32_t *)array - 2;
-}
+#define da_push(a, e)                   _da_insert((void **)&a, &e, sizeof(e), da_size(a))
+#define da_insert(a, e, i)              _da_insert((void **)&a, &e, sizeof(e), i)
+#define da_pop(a)                       if(da_size(a)) { _da_erase((void **)&a, sizeof(a[0]), da_size(a) - 1); }
+#define da_erase(a, i)                  if(da_size(a)) { _da_erase((void **)&a, sizeof(a[0]), i); }
 
-inline uint32_t da_size(void *array) {
-    if(array) {
-        return _da_raw(array)[0];
-    }
-    return 0;
-}
+#define da_free(a)                      { if(a) { free(da_raw(a)); a = NULL; } }
 
-inline uint32_t da_cap(void *array) {
-    if(array) {
-        return _da_raw(array)[1];
-    }
-    return 0;
-}
-
-inline void _da_free(void **array) {
-    if(*array) {
-        *array = (void *)(_da_raw(*array));
-    }
-    free(*array);
-    *array = NULL;
-}
-
-inline void _da_grow(void **array, size_t element_size) {
-    uint32_t new_cap = _DARRAY_START_CAP, size = 0;
-    bool need_realloc = true;
-    if(*array) {
-        size = da_size(*array);
-        if(size >= da_cap(*array)) {
-            new_cap = da_cap(*array) + (da_cap(*array) / 2);
+inline void _da_grow(void **array, size_t element_size, uint32_t required_elements) {
+    if(da_cap(*array) < required_elements) {
+        uint32_t new_cap = da_cap(*array) > _DARRAY_START_CAP ? da_cap(*array) : _DARRAY_START_CAP;
+        while(required_elements >= new_cap) {
+            new_cap = 3 * (new_cap / 2);
         }
-        else {
-            new_cap = da_cap(*array);
-            need_realloc = false;
-        }
+        *array = (void *)((uint32_t *)realloc(da_raw(*array), new_cap*element_size + 2*sizeof(uint32_t)) + 2);
+
+        *(da_raw(*array) + 1) = new_cap;
     }
-
-    if(need_realloc) {
-        if(*array) {
-            *array = (void *)_da_raw(*array);
-        }
-        *array = realloc(*array, (new_cap * element_size) + (2 * sizeof(uint32_t)));
-        *array = (void *)((uint32_t *)(*array) + 2);
-        _da_raw(*array)[0] = size;
-        _da_raw(*array)[1] = new_cap;
-    }
-}
-
-inline void _da_shrink(void **array, size_t element_size) {
-    if(*array) {
-        if(da_size(*array) > 0) {
-            if(da_size(*array) <= da_cap(*array) - (da_cap(*array) / 3)) {
-                _da_raw(*array)[1] = da_cap(*array) - (da_cap(*array) / 3);
-
-                *array = realloc(_da_raw(*array), (da_cap(*array) * element_size) + (2 * sizeof(uint32_t)));
-                *array = (uint32_t *)(*array) + 2;
-            }
-        }
-        else {
-            _da_free(array);
-        }
-    }
-}
-
-inline void _da_push(void **array, void *element, size_t element_size) {
-    _da_grow(array, element_size);
-
-    memcpy(((uint8_t *)(*array)) + (element_size * da_size(*array)),
-           element, element_size);
-    _da_raw(*array)[0]++;
 }
 
 inline void _da_insert(void **array, void *element, size_t element_size, uint32_t pos) {
-    _da_grow(array, element_size);
+    int8_t array_null = !*array;
+
+    _da_grow(array, element_size, da_size(*array) + 1);
+
+    if(array_null) {
+        da_raw(*array)[0] = 0;
+    }
 
     memmove(((uint8_t *)(*array)) + (element_size * (pos + 1)),
             ((uint8_t *)(*array)) + (element_size * pos),
@@ -192,18 +146,8 @@ inline void _da_insert(void **array, void *element, size_t element_size, uint32_
 
     memcpy(((uint8_t *)(*array)) + (element_size * pos),
            element, element_size);
-    _da_raw(*array)[0]++;
-}
 
-inline void _da_pop(void **array, size_t element_size) {
-    if(*array) {
-        _da_raw(*array)[0]--;
-
-        if(da_size(*array) < 1) {
-            da_free(*array);
-            *array = NULL;
-        }
-    }
+    da_raw(*array)[0]++;
 }
 
 inline void _da_erase(void **array, size_t element_size, uint32_t pos) {
@@ -212,7 +156,7 @@ inline void _da_erase(void **array, size_t element_size, uint32_t pos) {
                 ((uint8_t *)(*array)) + (element_size * (pos + 1)),
                 element_size * (da_size(*array) - pos - 1));
 
-        _da_raw(*array)[0]--;
+        da_raw(*array)[0]--;
 
         if(da_size(*array) < 1) {
             da_free(*array);
@@ -223,26 +167,31 @@ inline void _da_erase(void **array, size_t element_size, uint32_t pos) {
 
 #endif
 
-/* 
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 MIT License
 Copyright (c) 2017 Ryan Fleury
-Permission is hereby granted, free of charge, to any person 
-obtaining a copy of this software and associated documentation 
-files (the "Software"), to deal in the Software without 
-restriction, including without limitation the rights to use, copy, 
-modify, merge, publish, distribute, sublicense, and/or sell copies 
-of the Software, and to permit persons to whom the Software is 
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be 
-included in all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
-OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS 
-BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN 
-AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
-IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
-THE SOFTWARE.
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Permission is hereby granted, free of charge, to any
+person obtaining a copy of this software and associated
+documentation files (the "Software"), to deal in the
+Software without restriction, including without
+limitation the rights to use, copy, modify, merge,
+publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software
+is furnished to do so, subject to the following
+conditions: The above copyright notice and this permission
+notice shall be included in all copies or substantial
+portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
